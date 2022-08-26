e8d296587da452188fdcfbe97b95250a506aa73c
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-08-26 23:15:29 +0200

    add channelSearch and channelList

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -33,6 +33,12 @@ const ChannelListContainer = () => {
       <SideBar />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+        />
       </div>
     </>
   );

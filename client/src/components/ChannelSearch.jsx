1d83945890c9f5bfc1775da857222e07b04474e3
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-08-26 23:15:40 +0200

    update channelSearch

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useChatContext } from "stream-chat-react";
+import { getChannel, useChatContext } from "stream-chat-react";
 
 import { SearchIcon, searchIcon } from "../assets";
 
@@ -7,20 +7,30 @@ const ChannelSearch = () => {
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const getChannels = async (text) => {
+    try {
+      // TODO : fetch channels
+    } catch (error) {
+      setQuery("");
+    }
+  };
+
   const onSearch = (event) => {
     event.preventDefault();
 
     setLoading(true);
+    setQuery(event.target.value);
+    getChannels(event.target.value);
   };
 
   return (
     <div className="channel-search__container">
-      <div className="channell-search__input__wrapper">
+      <div className="channel-search__input__wrapper">
         <div className="channel-search__input__icon">
           <SearchIcon />
         </div>
         <input
-          className="channel-search__input__input"
+          className="channel-search__input__text"
           placeholder="Search"
           type="text"
           value={query}

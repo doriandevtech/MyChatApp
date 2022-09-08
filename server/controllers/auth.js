45813c80225d8dfb58cb1dc70b4cec0cd3a6209b
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-09-08 20:22:55 +0200

    update auth file

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,11 +3,21 @@ const bcrypt = require("bcrypt");
 const StreamChat = require("stream-chat");
 const crypto = require("crypto");
 
-const signup = (req, res) => {
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_API_ID;
+
+const signup = async (req, res) => {
   try {
     const { fullName, username, password, phoneNumber } = req.body;
 
     const userId = crypto;
+
+    const serverClient = connect(api_key, api_secret, app_id);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const token = serverClient.createUserToken(userId);
   } catch (error) {
     console.log(error);
 

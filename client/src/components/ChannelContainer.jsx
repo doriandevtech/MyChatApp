27dd074aa2d2dd35597cdd8c5d68f8fff391fa21
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-09-26 15:29:12 +0200

    update channelContainer component

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useContext } from "react";
+import { Channel, useChatContext } from "stream-chat-react";
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
 
 const ChannelContainer = () => {
+  const { channel } = useContext();
+
+  if (isCreating) {
+  }
+
   return <div>ChannelContainer</div>;
 };
 

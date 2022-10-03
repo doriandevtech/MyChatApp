b47ca961a8ce14ce2813142cc1a1a09dd75bea10
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-10-03 14:15:22 +0200

    add toggle members to UserList

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -15,20 +15,37 @@ const ListContainer = ({ children }) => {
   );
 };
 
-const UserItem = () => {
+const UserItem = ({ user, setSelectedUsers }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    if (selected) {
+      setSelectedUsers((prevUsers) =>
+        prevUsers.filter((prevUsers) => prevUsers !== user.id)
+      );
+    } else {
+      setSelectedUsers((prevUsers) => [...prevUsers, user.id]);
+    }
+    setSelected((prevSelected) => !prevSelected);
+  };
+
   return (
-    <div className="user-item__wrapper">
+    <div className="user-item__wrapper" onClick={handleSelect}>
       <div className="user-item__name-wrapper">
-        <Avatar />
+        <Avatar image={user.image} name={user.fullName || user.id} size={32} />
+        <p className="user-item__name">{user.fullName || user.id}</p>
       </div>
+      {selected ? <InviteIcon /> : <div className="user-item__invite-empty" />}
     </div>
   );
 };
 
-const UserList = () => {
+const UserList = ({ setSelectedUsers }) => {
   const { client } = useChatContext();
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -37,11 +54,60 @@ const UserList = () => {
       setLoading(true);
 
       try {
-      } catch (error) {}
-    };
-  }, [filters]);
+        const response = await client.queryUsers(
+          {
+            id: { $ne: client.userID },
+          },
+          { id: 1 },
+          { limit: 8 }
+        );
 
-  return <ListContainer>UserList</ListContainer>;
+        if (response.users.length) {
+          setUsers(response);
+        }
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    if (client) getUsers();
+  }, []);
+
+  if (error) {
+    return (
+      <ListContainer>
+        <div className="user-list__message">
+          Error loading, please refresh and try again.
+        </div>
+        f
+      </ListContainer>
+    );
+  }
+
+  if (listEmpty) {
+    return (
+      <ListContainer>
+        <div className="user-list__message">No users found.</div>f
+      </ListContainer>
+    );
+  }
+
+  return (
+    <ListContainer>
+      {loading ? (
+        <div className="user-list__message">Loading users...</div>
+      ) : (
+        users?.map((user, i) => (
+          <UserItem
+            index={i}
+            key={user.id}
+            user={user}
+            setSelectedUsers={setSelectedUsers}
+          />
+        ))
+      )}
+    </ListContainer>
+  );
 };
 
 export default UserList;

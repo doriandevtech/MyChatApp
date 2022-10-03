b7132eecaa22059f9c8deaf75b957161b06209e8
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-10-03 12:08:31 +0200

    add UserItem + ListContainer component

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,10 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, useChatChannel } from "stream-chat-react";
+import { Avatar, useChatContext } from "stream-chat-react";
 
 import { InviteIcon } from "../assets";
 
+const ListContainer = ({ children }) => {
+  return (
+    <div className="user-list__container">
+      <div className="user-list__header">
+        <p>User</p>
+        <p>Invite</p>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const UserItem = () => {
+  return (
+    <div className="user-item__wrapper">
+      <div className="user-item__name-wrapper">
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
 const UserList = () => {
-  return <div>UserList</div>;
+  const { client } = useChatContext();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if (loading) return;
+
+      setLoading(true);
+
+      try {
+      } catch (error) {}
+    };
+  }, [filters]);
+
+  return <ListContainer>UserList</ListContainer>;
 };
 
 export default UserList;

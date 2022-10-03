71abfd06a9e77c39c042ddf3c4f856c587995513
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-10-03 11:00:47 +0200

    create CreateChannel component

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
 
-function CreateChannel() {
-  return <div>CreateChannel</div>;
+import { UserList } from "./";
+import { ClassCreateChannel, CloseCreateChannel } from "../assets";
+import { useState } from "react";
+
+const ChannelNameInput = ({ channelName = "", setChannelName }) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setChannelName(event.target.value);
+  };
+
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input
+        value={channelName}
+        onChange={handleChange}
+        placeholder="channel-name"
+      />
+      <p>Add Members</p>
+    </div>
+  );
+};
+
+function CreateChannel({ createType, setIsCreating }) {
+  const [channelName, setChannelName] = useState("");
+
+  return (
+    <div className="create-channel__conatainer">
+      <div className="create_channel__header">
+        <p>
+          {createType === "team"
+            ? "Create a New Channel"
+            : "Send a direct message"}
+        </p>
+        <CloseCreateChannel setIsCreating={setIsCreating} />
+      </div>
+      {createType === "team" && (
+        <ChannelNameInput
+          channelName={channelName}
+          setChannelName={setChannelName}
+        />
+      )}
+      <UserList />
+    </div>
+  );
 }
 
 export default CreateChannel;
